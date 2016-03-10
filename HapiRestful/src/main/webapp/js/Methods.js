6e0c0605232f86acc89e52350970462125abaeed
Marco <marco.reiter95@gmail.com>
2016-03-10 15:57:54 +0100

    LoadPatients erste Tests

--- HapiRestful/src/main/webapp/js/Methods.js
+++ HapiRestful/src/main/webapp/js/Methods.js
@@ -1,7 +1,7 @@
 /**
  * 
  */
-var url = "http://127.0.0.1:8080";
+var url = "http://localhost:8081";
 
 
 function getPatientbyName() {
@@ -24,27 +24,40 @@ function getPatientbyName() {
 	url = url+method+param;
 	
     var xmlRequest = new XMLHttpRequest();
-    xmlRequest.open( "GET", url, true );
-    xmlRequest.send( null );
-    return xmlRequest.responseXML;
+    xmlRequest.open( "GET", url, false );
+    xmlRequest.withCredentials = true;
+    xmlRequest.send();
+   console.log(xmlRequest.responseXML);
+   
+
+	
     
 	}
 	
 }
-
+//gefundene Patienten in List anzeigen
+function loadPatients() {
+	
+	var list = document.getElementById('demo');
+	
+	var entry = document.createElement('li')
+	entry.appendChild(document.createTextNode("Test" + Math.random()));
+	list.appendChild(entry);
+}
+//
 function addNewPatient() {
 	
 	var method = "/HapiRestful/hapiservlet/"
 		
-	var svnValue = document.getElementById("svn_value");
+	var svnValue = document.getElementById('svn_value');
 	
-	var lastnameValue = document.getElementById("lastnameAdd");
+	var lastnameValue = document.getElementById('lastnameAdd');
 	
-	var firstnameValue =document.getElementById("firstnameAdd");
+	var firstnameValue =document.getElementById('firstnameAdd');
 	
-	var genderValue = document.getElementById("genderAdd");
+	var genderValue = document.getElementById('genderAdd');
 	
-	var locationValue = document.getElementById("locationAdd")
+	var locationValue = document.getElementById('locationAdd');
 	
 	if(lastnameValue.value == "" || firstnameValue.value == "" || svnValue.value == ""){
 		alert("Bitte alle Pflichteingaben abschließen!")
@@ -54,8 +67,10 @@ function addNewPatient() {
 		var xmlRequest = new XMLHttpRequest();
 		
 		xmlRequest.open("POST",url);
+		xmlRequest.withCredentials = true;
 		xmlRequest.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-		console.log(xmlRequest.send(JSON.stringify({value: svnValue, family: lastnameValue, given:firstnameValue, gender: genderValue, location : locationValue})));
+		xmlRequest.send(JSON.stringify({value: svnValue, family: lastnameValue, given:firstnameValue, gender: genderValue, location : locationValue}));
 		
+
 	}
 }
